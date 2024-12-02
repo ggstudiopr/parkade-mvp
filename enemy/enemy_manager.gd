6191c245018bcab17e580cacae5c89d2829999ba
Guihurt <gguihurt@gmail.com>
2024-12-02 03:44:44 -0400

    Simple Enemy and Enemy Manager

---
+++ enemy/enemy_manager.gd
@@ -0,0 +1,26 @@
+extends Node
+class_name EnemyManager
+
+var enemies: Array[Enemy]
+
+#TODO: REFACTOR INTO THE PLAYER NODE
+enum PLAYER_STATE {
+	WALKING,
+	DRIVING
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	enemies = get_children() as Array[Enemy]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func on_player_state_change(new_state):
+	for e in enemies:
+		match e.type:
+			Enemy.ENEMY_TYPE.CHASER:
+					e.change_state(Enemy.ENEMY_STATE.SEARCHING if new_state == PLAYER_STATE.WALKING else Enemy.ENEMY_STATE.WAITING)
+			Enemy.ENEMY_TYPE.VIBER:
+					e.change_state(Enemy.ENEMY_STATE.SEARCHING if new_state == PLAYER_STATE.DRIVING else Enemy.ENEMY_STATE.WAITING)
