5f5dd4c6af787ad070cc0975a595bf9c67415942
GR47 <guillermo.rosarcab@gmail.com>
2025-02-09 07:39:55 -0800

    car node updated with more features, only missing y-axis controls atp

--- basic_player.gd
+++ basic_player.gd
@@ -32,6 +32,14 @@ var _camera_rotation : Vector3
 @onready var VEHICLE := $"../Vehicle"
 @onready var VEHICLE_LABEL := $"../Vehicle/VehicleLabel"
 var _is_near_car : bool
+@export var CAR_CAM_TURN_UPPER_LIMIT := deg_to_rad(30)
+@export var CAR_CAM_TURN_LOWER_LIMIT := deg_to_rad(-30)
+@export var CAR_CAM_TILT_UPPER_LIMIT := deg_to_rad(15)
+@export var CAR_CAM_TILT_LOWER_LIMIT := deg_to_rad(-25)
+@export var CAR_SPEED_DEFAULT = 7
+@export var CAR_BRAKE_RATE = 0.4
+@export var CAR_SPEED_ACCEL = 10
+@export var CAR_TURN_SPEED = 0.7
 
 #BOOLS FOR MOVEMENT/ANIMATION LOGIC
 var _is_crouching : bool
@@ -94,85 +102,105 @@ func check_interact():
 			if _is_crouching == true:
 				crouch_animation()
 				await get_tree().create_timer(1.0).timeout
-			#global_rotation = VEHICLE.global_rotation
 			VEHICLE_LABEL.text = str("ENTERED")
 		elif state == PLAYER_STATE.WALKING:
 			VEHICLE_LABEL.text = str("EXITED")
-			
 #Rotates Camera Controller based on mouse input
 func _update_camera(delta):
 	_mouse_rotation.x += _tilt_input * delta
-	_mouse_rotation.y += _rotation_input * delta
-	if state == PLAYER_STATE.WALKING: 
+	if state == PLAYER_STATE.WALKING: #base x axis camera clamp on walk state
+		_mouse_rotation.y += _rotation_input * delta
 		_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
-	#elif state == PLAYER_STATE.DRIVING: #limited camera control when sitting in car
-		#_mouse_rotation.x = clamp(_mouse_rotation.x, deg_to_rad(-60), deg_to_rad(60))
-		#_mouse_rotation.y = clamp(_mouse_rotation.y, deg_to_rad(-45), deg_to_rad(45))
 		
+	elif state == PLAYER_STATE.DRIVING: #limited camera control when sitting in car	
+		_mouse_rotation.x = clamp(_mouse_rotation.x, CAR_CAM_TILT_LOWER_LIMIT, CAR_CAM_TILT_UPPER_LIMIT)
+		'''
+		the below code is in_progress to control y-axix (left-right) while in vehicle
+		a basic clamp wont work because rotation is in radians and when the values 
+		cross from -3.14>3.14, the clamp just kills itself in logic
+		
+		an alternative that helps keep limited control of camera based on vehicle orientation
+		remains to be made or figured out
+		
+		heres a reddit link that kinda has the same issue 
+		https://www.reddit.com/r/godot/comments/188rlpf/help_with_limiting_rotation_within_two_angles/
+		
+		it is 7:30am and i cant be fucked anymore 
+		
+		-GAR	2/9/2025
+		
+		var RIGHT_LIM = CAR_CAM_TURN_LOWER_LIMIT + VEHICLE.rotation.y 
+		RIGHT_LIM = RIGHT_LIM  - 2 * PI * floor(RIGHT_LIM  / (2 * PI))
+		var LEFT_LIM = CAR_CAM_TURN_UPPER_LIMIT + VEHICLE.rotation.y 
+		LEFT_LIM = LEFT_LIM  - 2 * PI * floor(LEFT_LIM / (2 * PI))
+		var MOUSE_ROT = _mouse_rotation.y  - 2 * PI * floor(_mouse_rotation.y  / (2 * PI))
+		
+		RIGHT_LIM += 10
+		LEFT_LIM += 10
+		MOUSE_ROT += 10
+		
+		print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		print("L: ",LEFT_LIM)
+		print("m: ",MOUSE_ROT)
+		print("R: ",RIGHT_LIM)
+		if ((MOUSE_ROT > LEFT_LIM or MOUSE_ROT < RIGHT_LIM)):
+			_mouse_rotation.y += _rotation_input * delta
+		#figure out above
+		_mouse_rotation.x = clamp(_mouse_rotation.x, CAR_CAM_TILT_LOWER_LIMIT, CAR_CAM_TILT_UPPER_LIMIT)
+		'''
 	_player_rotation = Vector3(0, _mouse_rotation.y, 0)
 	_camera_rotation = Vector3(_mouse_rotation.x, 0, 0)
-	
-	#UPDATE PLAYER CAMERA ROT
 	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
 	CAMERA_CONTROLLER.rotation.z = 0
 	global_transform.basis = Basis.from_euler(_player_rotation)
-	
 	_rotation_input = 0
 	_tilt_input = 0
-
+	
 func _process(delta) -> void:
 	Global.player_position = global_position
 
 func _physics_process(delta: float) -> void:
-	#UPDATE CAM
 	_update_camera(delta)
-	#JUMP
 	if not is_on_floor() and state ==  PLAYER_STATE.WALKING:
 		velocity += get_gravity() * delta
 	if Input.is_action_just_pressed("jump") and is_on_floor() and _is_crouching == false and state ==  PLAYER_STATE.WALKING:
 		velocity.y = JUMP_VELOCITY
-	#WASD MOVE + SPRINT
+
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction and state == PLAYER_STATE.WALKING: #movement input for walking
-		velocity.x = direction.x * _speed
-		velocity.z = direction.z * _speed
-		#simple sprint speed mult only when standing and not mid jump, check headbobbing for auditory/visual cues
-		if Input.is_action_pressed("sprint") and is_on_floor() and _is_crouching == false and  state ==  PLAYER_STATE.WALKING:
-			velocity.z *= SPRINT_MULT
-			_is_sprinting = true
+	
+	if state == PLAYER_STATE.WALKING:
+		if direction: 
+			velocity.x = direction.x * _speed
+			velocity.z = direction.z * _speed
+			if Input.is_action_pressed("sprint") and is_on_floor() and _is_crouching == false:
+				velocity.z *= SPRINT_MULT
+				_is_sprinting = true
+			else:
+				_is_sprinting = false 
 		else:
-			_is_sprinting = false 
-	elif state == PLAYER_STATE.WALKING: #deccel for walking
-		velocity.x = move_toward(velocity.x, 0, _speed)
-		velocity.z = move_toward(velocity.z, 0, _speed)
-	
-	if state == PLAYER_STATE.DRIVING:
-		#turn car left and right when moving forward/backward
-		if (Input.is_action_pressed("move_right") and Input.is_action_pressed("move_forward")) or (Input.is_action_pressed("move_left") and Input.is_action_pressed("move_backward")):
-			VEHICLE.rotate_y(-1*delta)
-		elif (Input.is_action_pressed("move_left") and Input.is_action_pressed("move_forward")) or ( Input.is_action_pressed("move_right") and Input.is_action_pressed("move_backward")):
-			VEHICLE.rotate_y(1*delta)
-		#move back/forth
-		if direction and Input.is_action_pressed("move_forward"):
-			VEHICLE.velocity += -(VEHICLE.transform.basis.z) * _speed * 0.1
-		elif direction and Input.is_action_pressed("move_backward"):
-			VEHICLE.velocity += (VEHICLE.transform.basis.z) * _speed * 0.1
-		#stop car when input release
-		else: #stops car movement
-			VEHICLE.velocity.x = move_toward(VEHICLE.velocity.x, 0, _speed)
-			VEHICLE.velocity.z = move_toward(VEHICLE.velocity.z, 0, _speed)
-	
-		VEHICLE.move_and_slide()
-		global_position = VEHICLE.global_position #clamp player to car as it moves
+			velocity.x = move_toward(velocity.x, 0, _speed)
+			velocity.z = move_toward(velocity.z, 0, _speed)
 
-	#HEADBOB ANIM, HARDCODED LEFT RIGHT SWAY
-	#if input_dir.x>0:
-		#rotation.z = lerp_angle(rotation.z, deg_to_rad(-3), 0.05)
-	#elif input_dir.x<0:
-		#rotation.z = lerp_angle(rotation.z, deg_to_rad(3), 0.05)
-	#else:
-		#rotation.z = lerp_angle(rotation.z, deg_to_rad(0), 0.05)
+	if state == PLAYER_STATE.DRIVING:
+		if direction: 
+			VEHICLE.velocity = Vector3.ZERO
+			if (((Input.is_action_pressed("move_forward") and Input.is_action_pressed("move_right")) or Input.is_action_pressed("move_backward") and Input.is_action_pressed("move_left"))):
+				_mouse_rotation.y += -CAR_TURN_SPEED*delta
+				VEHICLE.rotate_y(-CAR_TURN_SPEED*delta)
+			elif (((Input.is_action_pressed("move_forward") and Input.is_action_pressed("move_left")) or Input.is_action_pressed("move_backward") and Input.is_action_pressed("move_right"))):
+				_mouse_rotation.y += CAR_TURN_SPEED*delta
+				VEHICLE.rotate_y(CAR_TURN_SPEED*delta)
+			if (Input.is_action_pressed("move_forward")):
+				VEHICLE.velocity += -(VEHICLE.transform.basis.z) * CAR_SPEED_DEFAULT
+			elif (Input.is_action_pressed("move_backward")):
+				VEHICLE.velocity += (VEHICLE.transform.basis.z) * CAR_SPEED_DEFAULT
+		else: #deccel
+			VEHICLE.velocity.x = move_toward(VEHICLE.velocity.x, 0, CAR_BRAKE_RATE)
+			VEHICLE.velocity.z = move_toward(VEHICLE.velocity.z, 0, CAR_BRAKE_RATE)
+		VEHICLE.move_and_slide()
+		global_position = VEHICLE.global_position
+
 	#GENERIC HEADBOB ANIM ON MOVEMENT W/ CONDITIONAL ON BOOLS
 	if (input_dir.y>0 or input_dir.y<0) and _is_crouching == false and state == PLAYER_STATE.WALKING:
 		if _is_sprinting == true:
